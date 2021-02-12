2ff1942cee50ade7f9e66e15674fbf2c7ca672f8
Hind <hindfathi.dev@gmail.com>
2021-02-12 23:46:05 +0100

    add social icone

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@
         $(function () {
         $('[data-toggle="popover"]').popover()
         })
+                    if(screen.width == 1440)
+            {
+ 
+               document.getElementById("fa-icones").style.display='none';
+               document.getElementById("go-icones").style.display='none';
+            }
+            if(screen.width == 1024)
+            {
+ 
+               document.getElementById("fa-icones").style.display='none';
+               document.getElementById("go-icones").style.display='none';
+            }
