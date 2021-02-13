1e7969d911af0d27b747bddae60493c573ee85db
Hind <hindfathi.dev@gmail.com>
2021-02-13 13:34:11 +0100

    add fixed bar social

--- js/script.js
+++ js/script.js
@@ -7,17 +7,27 @@
         $(function () {
         $('[data-toggle="popover"]').popover()
         })
+                            if(screen.width == 1492)
+            {
+ 
+               document.getElementById("fa-icones").style.display='none';
+               document.getElementById("go-icones").style.display='none';
+            }
                     if(screen.width == 1440)
             {
  
                document.getElementById("fa-icones").style.display='none';
                document.getElementById("go-icones").style.display='none';
+               document.getElementById("tw-icones").style.display='none';
+
+
             }
             if(screen.width == 1024)
             {
  
                document.getElementById("fa-icones").style.display='none';
                document.getElementById("go-icones").style.display='none';
+               document.getElementById("tw-icones").style.display='none';
             }
             if(screen.width == 1536)
             {
@@ -29,6 +39,7 @@
             {
  
                document.getElementById(".sticky-container").style.right='537px';
+
             }
             if(screen.width == 425)
             {
